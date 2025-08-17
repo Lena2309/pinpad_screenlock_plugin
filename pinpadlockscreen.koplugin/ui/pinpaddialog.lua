cba5bc3683ddaf5e5bb58a34ce8c0f146fdc46c5
Lena2309 <lena.bamouh@etu.univ-nantes.fr>
2025-08-17 17:41:18 +0200

    feat: adding the PIN Pad on Screensaver background Resolve #2

--- pinpadlockscreen.koplugin/ui/pinpaddialog.lua
+++ pinpadlockscreen.koplugin/ui/pinpaddialog.lua
@@ -4,12 +4,22 @@ Description: represents a PIN pad interface and its behavior within KOReader.
 It handles user input, PIN validation, and UI interactions.
 ]]
 
+local Blitbuffer = require("ffi/blitbuffer")
+local BookStatusWidget = require("ui/widget/bookstatuswidget")
 local Button = require("ui/widget/button")
-local PinPadButtonDialog = require("ui/pinpadbuttondialog")
+local Device = require("device")
 local FrameContainer = require("ui/widget/container/framecontainer")
 local InfoMessage = require("ui/widget/infomessage")
+local ImageWidget = require("ui/widget/imagewidget")
+local PinPadButtonDialog = require("ui/pinpadbuttondialog")
+local RenderImage = require("ui/renderimage")
+local Screensaver = require("ui/screensaver")
+local ScreenSaverWidget = require("ui/widget/screensaverwidget")
 local UIManager = require("ui/uimanager")
+local ffiUtil = require("ffi/util")
+local util = require("util")
 local _ = require("gettext")
+local Screen = Device.screen
 
 local LOCKED_TEXT = _("Enter your PIN")
 local CONFIRM_CURRENT_CODE_TEXT = _("Enter the current PIN Code")
@@ -37,6 +47,11 @@ function PinPadDialog:init()
     self:setDialogText()
     self.pin = ""
     self.buttons = self:initializeButtons()
+    Screensaver:setup()
+    self:initializeScreensaverBackground()
+    if self.screensaver_widget then
+        UIManager:show(self.screensaver_widget)
+    end
     return self
 end
 
@@ -84,6 +99,113 @@ function PinPadDialog:initializeButtons()
         }
     }
     return buttons
+end
+
+function PinPadDialog:initializeScreensaverBackground()
+    if Screensaver.screensaver_type == "disable" and not Screensaver.show_message and not Screensaver.overlay_message then
+        return
+    end
+
+    local rotation_mode = Screen:getRotationMode()
+
+    if Screensaver:modeExpectsPortrait() then
+        Device.orig_rotation_mode = rotation_mode
+        -- Leave Portrait & Inverted Portrait alone, that works just fine.
+        if bit.band(Device.orig_rotation_mode, 1) == 1 then
+            -- i.e., only switch to Portrait if we're currently in *any* Landscape orientation (odd number)
+            Screen:setRotationMode(Screen.DEVICE_ROTATED_UPRIGHT)
+        else
+            Device.orig_rotation_mode = nil
+        end
+
+        -- On eInk, if we're using a screensaver mode that shows an image,
+        -- flash the screen to white first, to eliminate ghosting.
+        if Device:hasEinkScreen() and Screensaver:modeIsImage() then
+            if Screensaver:withBackground() then
+                Screen:clear()
+            end
+            Screen:refreshFull(0, 0, Screen:getWidth(), Screen:getHeight())
+
+            -- On Kobo, on sunxi SoCs with a recent kernel, wait a tiny bit more to avoid weird refresh glitches...
+            if Device:isKobo() and Device:isSunxi() then
+                ffiUtil.usleep(150 * 1000)
+            end
+        end
+    else
+        -- nil it, in case user switched ScreenSaver modes during our lifetime.
+        Device.orig_rotation_mode = nil
+    end
+
+    local widget = nil
+    if Screensaver.screensaver_type == "cover" or Screensaver.screensaver_type == "random_image" then
+        local widget_settings = {
+            width = Screen:getWidth(),
+            height = Screen:getHeight(),
+            scale_factor = G_reader_settings:isFalse("screensaver_stretch_images") and 0 or nil,
+            stretch_limit_percentage = G_reader_settings:readSetting("screensaver_stretch_limit_percentage"),
+        }
+        if Screensaver.image then
+            widget_settings.image = Screensaver.image
+            widget_settings.image_disposable = true
+        elseif Screensaver.image_file then
+            if G_reader_settings:isTrue("screensaver_rotate_auto_for_best_fit") then
+                -- We need to load the image here to determine whether to rotate
+                if util.getFileNameSuffix(Screensaver.image_file) == "svg" then
+                    widget_settings.image = RenderImage:renderSVGImageFile(Screensaver.image_file, nil, nil, 1)
+                else
+                    widget_settings.image = RenderImage:renderImageFile(Screensaver.image_file, false, nil, nil)
+                end
+                if not widget_settings.image then
+                    widget_settings.image = RenderImage:renderCheckerboard(Screen:getWidth(), Screen:getHeight(),
+                        Screen.bb:getType())
+                end
+                widget_settings.image_disposable = true
+            else
+                widget_settings.file = Screensaver.image_file
+                widget_settings.file_do_cache = false
+            end
+            widget_settings.alpha = true
+        end                                               -- set cover or file
+        if G_reader_settings:isTrue("screensaver_rotate_auto_for_best_fit") then
+            local angle = rotation_mode == 3 and 180 or 0 -- match mode if possible
+            if (widget_settings.image:getWidth() < widget_settings.image:getHeight()) ~= (widget_settings.width < widget_settings.height) then
+                angle = angle + (G_reader_settings:isTrue("imageviewer_rotation_landscape_invert") and -90 or 90)
+            end
+            widget_settings.rotation_angle = angle
+        end
+        widget = ImageWidget:new(widget_settings)
+    elseif Screensaver.screensaver_type == "bookstatus" then
+        local ReaderUI = require("apps/reader/readerui")
+        widget = BookStatusWidget:new {
+            ui = ReaderUI.instance,
+            readonly = true,
+        }
+    elseif Screensaver.screensaver_type == "readingprogress" then
+        widget = Screensaver.getReaderProgress()
+    end
+
+    -- Assume that we'll be covering the full-screen by default (either because of a widget, or a background fill).
+    local covers_fullscreen = true
+    -- Speaking of, set that background fill up...
+    local background
+    if Screensaver.screensaver_background == "black" then
+        background = Blitbuffer.COLOR_BLACK
+    elseif Screensaver.screensaver_background == "white" then
+        background = Blitbuffer.COLOR_WHITE
+    elseif Screensaver.screensaver_background == "none" then
+        background = nil
+    end
+
+    UIManager:setIgnoreTouchInput(false)
+
+    if widget then
+        self.screensaver_widget = ScreenSaverWidget:new {
+            widget = widget,
+            background = background,
+            covers_fullscreen = covers_fullscreen,
+        }
+        self.screensaver_widget.dithered = true
+    end
 end
 
 function PinPadDialog:isBlocked()
@@ -166,6 +288,10 @@ end
 function PinPadDialog:close()
     self:reset()
     self:closeDialogs()
+    if self.screensaver_widget then
+        UIManager:close(self.screensaver_widget)
+        self.screensaver_widget = nil
+    end
 end
 
 function PinPadDialog:onAppendToPin(digit)
