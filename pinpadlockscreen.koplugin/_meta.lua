4ecd77b42603550558fe05e10817ff597b8fc318
Lena2309 <lena.bamouh@etu.univ-nantes.fr>
2025-08-19 18:11:44 +0200

    version: new patch for fix

--- pinpadlockscreen.koplugin/_meta.lua
+++ pinpadlockscreen.koplugin/_meta.lua
@@ -4,5 +4,5 @@ return {
     name = "pinpadlock",
     fullname = _("PIN Pad Lock Screen"),
     description = _([[Lock the device with a PIN code]]),
-    version = "v1.0.1"
+    version = "v1.0.2"
 }
